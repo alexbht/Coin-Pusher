56b897e2910e64575529f281e16f50e6c82391f0
Alexandre <alexandre.berthelot@edu.esiee.fr>
2025-04-24 00:40:29 +0200

    Polished draft
    Added multiple stacks and greyboxing of the coin pusher

--- Assets/MiniGames/Project/Script/Pusher.cs
+++ Assets/MiniGames/Project/Script/Pusher.cs
@@ -2,18 +2,20 @@ using UnityEngine;
 
 public class Pusher : MonoBehaviour
 {
+    private float originalZ;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        originalZ = transform.position.z;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float dT = Time.time;
         Vector3 pos = transform.position;
         Rigidbody rb = GetComponent<Rigidbody>();
-        pos.z = Mathf.Sin((2*Mathf.PI/ 5) * dT );
+        pos.z = originalZ + Mathf.Sin((2*Mathf.PI/ 5) * dT );
         rb.MovePosition(pos); 
     }
 }
